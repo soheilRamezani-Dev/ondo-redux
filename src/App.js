a6d9a852f44a4dae7b935b2f90009948452ef4fb
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-21 08:53:06 +0430

    show task template complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import AddDoForm from "./add-do/AddDoForm";
+import AddDoForm from "./components/add-do/AddDoForm";
+import ShowTasks from "./components/show-tasks/ShowTasks";
 const App = () => {
   return (
     <div className="container p-5">
       <div className="row">
         <AddDoForm />
+        <ShowTasks />
       </div>
     </div>
   );

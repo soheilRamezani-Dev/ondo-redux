a57175a3a398121fc704f049101e8c108a5100a0
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-21 08:43:15 +0430

    add form component template for add task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import AddDoForm from "./add-do/AddDoForm";
 const App = () => {
-    return <>fgbh</>
-}
- 
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <AddDoForm />
+      </div>
+    </div>
+  );
+};
+
 export default App;

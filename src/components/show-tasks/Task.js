0f4e1e758f74e14986a8a96eac3b0fded01090fd
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-26 09:06:08 +0330

    icons added to project

--- src/components/show-tasks/Task.js
+++ src/components/show-tasks/Task.js
@@ -1,11 +1,17 @@
-const Task = ({situation,id,title,date}) => {
+import "./task.css";
+import {FaCheck,FaTimes} from 'react-icons/fa'
+const Task = ({ situation, id, title, date }) => {
   return (
-    <tr className={`alert alert-${situation?'success':'danger'}`}>
+    <tr className={` m-2 alert alert-${situation ? "success" : "danger"}`}>
       <td>{id}</td>
       <td>{title}</td>
       <td>{date}</td>
-      <td>{situation?'completed':''}</td>
-      <td>no actions yet</td>
+      <td>{situation ? "completed" : ""}</td>
+      <td className="d-flex align-items-center">
+        {situation
+          ? <span role="button" className="border border-success rounded p-2"><FaTimes size={20}/></span>
+          : <span role="button" className="border border-danger rounded p-2"><FaCheck size={20}/></span>}
+      </td>
     </tr>
   );
 };
